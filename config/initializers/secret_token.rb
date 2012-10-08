608a697af0e675122f538457a1a1f58b45645dcd
user <user@test.ru>
2012-10-08 19:57:59 +0400

    dfdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRTest::Application.config.secret_token = '29f1553f9f921c6622b7a6c476841191d79dfdb2b5babfa19d19d8532e70f6e7c594b0e6bfa9a88ba2a2af72b401e0f0412e98f32f47c63dced55c998edecacc'
